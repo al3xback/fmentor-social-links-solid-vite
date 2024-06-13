0a7070179ca8f3bf7940a945231290eabb726998
alex <al3xback@gmail.com>
2024-06-13 13:52:42 +0700

    changed classname to class, minimized class

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,24 +3,18 @@ import styles from './Profile.module.css';
 
 export default function Profile() {
 	return (
-		<div className={styles.profile}>
-			<img
-				src={avatarImg}
-				className={styles.image}
-				width={80}
-				height={80}
-				alt='Jessica Randall'
-			/>
-			<div className={styles['title-box']}>
-				<h2 className={styles.title}>Jessica Randall</h2>
-				<p className={styles.location}>London, United Kingdom</p>
+		<div class={styles.profile}>
+			<img src={avatarImg} width={80} height={80} alt='Jessica Randall' />
+			<div class={styles['title-box']}>
+				<h2>Jessica Randall</h2>
+				<p>London, United Kingdom</p>
 			</div>
 			<p>&Prime;Front-end developer and avid reader.&Prime;</p>
-			<ul className={styles['social-links']}>
+			<ul class={styles['social-links']}>
 				<li>
 					<a
 						href='#'
-						className='btn btn--primary btn--full'
+						class='btn btn--primary btn--full'
 						rel='noopener'
 						target='_blank'
 					>
@@ -30,7 +24,7 @@ export default function Profile() {
 				<li>
 					<a
 						href='#'
-						className='btn btn--primary btn--full'
+						class='btn btn--primary btn--full'
 						rel='noopener'
 						target='_blank'
 					>
@@ -40,7 +34,7 @@ export default function Profile() {
 				<li>
 					<a
 						href='#'
-						className='btn btn--primary btn--full'
+						class='btn btn--primary btn--full'
 						rel='noopener'
 						target='_blank'
 					>
@@ -50,7 +44,7 @@ export default function Profile() {
 				<li>
 					<a
 						href='#'
-						className='btn btn--primary btn--full'
+						class='btn btn--primary btn--full'
 						rel='noopener'
 						target='_blank'
 					>
@@ -60,7 +54,7 @@ export default function Profile() {
 				<li>
 					<a
 						href='#'
-						className='btn btn--primary btn--full'
+						class='btn btn--primary btn--full'
 						rel='noopener'
 						target='_blank'
 					>
